e3ed5d1bb35610cc65d9d74aa363641ebd3da2e1
St4YZ <57144223+St4YZ@users.noreply.github.com>
2023-06-02 13:22:46 +0300

    test commit message

--- cypress/integration/oldTests/Test4.js
+++ cypress/integration/oldTests/Test4.js
@@ -2,28 +2,27 @@
 
 describe('My 4th Test', () => {
     it('My 4th Test Case', () => {
-        
-        cy.visit(Cypress.env('url')+'/AutomationPractice/')
+
+        cy.visit(Cypress.env('url') + '/AutomationPractice/')
         cy.get('#alertbtn').click()
         cy.get('[value="Confirm"]').click()
         // window:alert
-        cy.on('window:alert',(str) =>
-        {
+        cy.on('window:alert', (str) => {
             // Mocha
             expect(str).to.equal('Hello , share this practice page and share your knowledge')
         })
         // window:confirm
-        cy.on('window:confirm',(str) =>
-        {
+        cy.on('window:confirm', (str) => {
             // Mocha
             expect(str).to.equal('Hello , Are you sure you want to confirm?')
         })
         // Switch tab Example
-        cy.get('#opentab').invoke('removeAttr','target').click()
-        cy.url().should('include','rahulshettyacademy')
+        cy.get('#opentab').invoke('removeAttr', 'target').click()
+        cy.url().should('include', 'rahulshettyacademy')
         cy.go('back')
-        cy.url().should('include','rahulshettyacademy')
+        cy.url().should('include', 'rahulshettyacademy')
+        cy.visit('google.com')
 
 
     })
-  })
+})
